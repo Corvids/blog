1d42e0788926ca17840cdf0da419bc513d2cd0ee
Jennifer Joo <jenjoo@microsoft.com>
2019-11-18 21:13:58 -0800

    update single-code-snippet

--- src/components/single-code-snippet.js
+++ src/components/single-code-snippet.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "./layout"
+import SEO from "./seo"
 
 import "../style/snippet.css"
 
 const SingleSnippet = (snippet_title, link_to, desc) => (
-    <Layout ClassName="">
+    <div ClassName="">
         <SEO title={snippet_title} />
         <p>{desc}</p>
         <Link to={link_to} className="link-to-home">View Code</Link>
-    </Layout>
+    </div>
 )
 
 export default SingleSnippet
--- src/pages/code-snippets.js
+++ src/pages/code-snippets.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import SingleSnippet from "../components/single-code-snippet"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const CodeSnippets = () => (
   <Layout>
     <SEO title="code snippets" />
-    <p>This is where you can find some of my code snippets.</p>
+    <SingleSnippet snippet_title="test" link_to="test" desc="test"/>
     <Link to="/" className="link-to-home">Go back to the homepage</Link>
   </Layout>
 )
