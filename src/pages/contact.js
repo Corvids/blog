d92f2a78b2db02623b1bee29cfa1f57cee4a5d33
Jennifer Joo <jenjoo@microsoft.com>
2020-07-13 20:35:20 -0700

    clean up form a bit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,32 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import "../style/contact.css" 
 
 const Contact = () => (
   <div>
     <SEO title="Contact" />
     <form method="post" action="https://getform.io/{your-unique-getform-endpoint}">
       <label>
-        Email:{" "}
-        <input type="email" name="email" />
+        email:
+        <br/>
+        <input type="email" name="email" className="text-box"/>
       </label>
       <br/>
       <label>
-        Name:{" "}
-        <input type="text" name="name" />
+        name:
+        <br/>
+        <input type="text" name="name" className="text-box"/>
       </label>
       <br/>
       <label>
-        Message:{" "}
-        <input type="text" name="message" />
+        message:
+        <br/>
+        <input type="text" name="message" className="message-box" />
       </label>
     </form>
+    <button type="button">Submit</button>
+    <br /><br/>
   </div>
 )
 
